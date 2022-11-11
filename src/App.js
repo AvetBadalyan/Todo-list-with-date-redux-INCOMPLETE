c3014453d3661fa3eff5312ae1011222797802b9
AvetBadalyan <avetbadalyan@gmail.com>
2022-11-11 15:30:31 +0400

    feat: create routing for home page and single task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import SingleTask from "./Pages/singleTask/SingleTask";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="single-task" element={<SingleTask />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
